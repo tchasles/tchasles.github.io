066a4317daffb8cd0c64f5f6081490b0c47966ab
tchasles <tchasles@gmail.com>
2016-01-26 13:27:17 +0100

    Doc

--- README.md
+++ README.md
@@ -3,4 +3,20 @@
 ## Overview
 Adneom News est une application AngularJS permettant de consulter, publier, commenter et liker des news
 
-##
+##Developmment
+
+Etape 1:
+Mise en place de l'application angular-newsAdneom
+
+Etape 2:
+Création dans le fichier controller.js d'un objet test ainsi que les fonction permettant de le modifier
+
+Etape 3:
+Préparation d'un index.html comprenant les diverse données à afficher ainsi que les éléments faisant appel au fonction (ajouter une news, ajouter un commentaire, upvoter une news)
+Sans prendre en compte la mise en page.
+
+Etape 4:
+Suppression de l'objet test dans le controller et ajout du service $http permettant l'acces à l'API Adneom.
+
+Etape 5:
+Ajout du fichie app.less et mise en forme de la page html
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,13 @@
 var newsApp = angular.module('newsApp', [,'angular.less']);
 /* Controllers */
 newsApp.controller('NewsListCtrl', function ($scope, $http) {
-    $scope.news = null;
+
+    //initialisation du model news à partir de l'API ADNEOM
     $scope.news = $http({method: "GET", url: "http://adneom.herokuapp.com/api/posts"}).
         then(function(response) {
             $scope.news = response.data;
+
+            //ajout pour chaque news des commentaires qui lui sont associé
             for(var i=0; i< $scope.news.length; i++){
                 $http({method: "GET", url: "http://adneom.herokuapp.com/api/posts/"+$scope.news[i]._id}).
                     then(function(response) {
@@ -19,13 +22,14 @@ newsApp.controller('NewsListCtrl', function ($scope, $http) {
             $scope.news = response.data;
         });
 
-
+    //retourne l'index de l'objet dont _id = id;
     function getIndexNews(id) {
         return $scope.news.map(function(el){
             return el._id;
         }).indexOf(id);
     };
 
+    //Upvotes la news dont _id = id
     $scope.addLikeTo = function(id) {
         $http({method: "PUT", url: "http://adneom.herokuapp.com/api/posts/"+id+"/upvote "}).
             then(function(response) {
@@ -34,6 +38,8 @@ newsApp.controller('NewsListCtrl', function ($scope, $http) {
                 response.data;
             });
     };
+
+    //Ajout d'un commentaire newComment à la news dont _id = id
     $scope.addCommentTo = function(id, newComment){
         $http({method: "POST", url: "http://adneom.herokuapp.com/api/posts/"+id+"/comments", data: newComment}).
             then(function(response) {
@@ -42,6 +48,8 @@ newsApp.controller('NewsListCtrl', function ($scope, $http) {
                 response.data;
             });
     }
+
+    //ajout d'une news
     $scope.addNews = function(news){
         $http({method: "POST", url: "http://adneom.herokuapp.com/api/posts", data: news}).
             then(function(response) {
